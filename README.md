76597a135970747d296e99c1326071760e55891e
Tom McKeesick <tmck01@gmail.com>
2021-08-01 16:03:02 +1000

    Update, parse report to JSON

--- README.md
+++ README.md
@@ -1,2 +1,11 @@
 # typeperf-wsl-go
+
 Making a bridge for using Windows hardware stats in WSL Linux
+
+## POC
+
+```shell
+go run main.go
+
+go run main.go | jq .
+```
--- main.go
+++ main.go
@@ -4,7 +4,30 @@ import (
 	"fmt"
 	"log"
 	"os/exec"
+	"strings"
+	"encoding/json"
 )
+
+type stat struct {
+	Key string
+    Typeperf_name string
+    Value  string
+	Vtype string
+}
+
+func newStat(report string, key string, stat_index int) *stat {
+	// split report and return new stat
+	lines := strings.Split(report, "\n")
+	header := strings.Split(lines[1], ",")
+	values := strings.Split(lines[2], ",")
+	s := stat{
+		Key: key,
+		Typeperf_name: strings.Trim(header[stat_index+1], "\r"),
+		Value: strings.Trim(values[stat_index+1], "\r"),
+		Vtype: "float",
+	}
+    return &s
+}
 
 func main() {
 	COMMANDS := map[string]string{
@@ -23,5 +46,11 @@ func main() {
 	if err != nil {
 		log.Fatal(err)
 	}
-	fmt.Println(string(out))
+	// fmt.Println("-- report:\n", string(out))
+
+	for i, s := range []string{"cpu", "mem_used", "disk_read", "disk_write"} {
+		stat := newStat(string(out), s, i)
+		json_stat, _ := json.Marshal(stat)
+		fmt.Println(string(json_stat))
+	}
 }
